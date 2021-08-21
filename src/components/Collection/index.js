df5e50bc398461003822d2bb905a70110b8166d0
catai <catai@edu.uwaterloo.ca>
2021-08-20 22:09:56 -0400

    Added starter page for the collection page,

---
+++ src/components/Collection/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Collection = () => {
+  return (
+    <div className="container h-screen relative z-20">
+      <div className="h-full flex flex-col justify-end pb-4 lg:pb-0 lg:w-96 lg:justify-center"></div>
+    </div>
+  );
+};
+
+export default Collection;
